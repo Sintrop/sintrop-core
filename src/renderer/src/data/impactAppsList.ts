1ea27b394a2bb1924688a654d75300de7af1c94e
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-07-23 14:30:24 -0300

    remove old impact apps

--- src/renderer/src/data/impactAppsList.ts
+++ src/renderer/src/data/impactAppsList.ts
@@ -1,4 +1,3 @@
-import GlobalPlantCatalogJson from '../../../impactApps/testnet/GlobalPlantCatalog.sol/GlobalPlantCatalog.json'
 import FreeEducationCenterJson from '../../../impactApps/testnet/FreeEducationCenter.sol/FreeEducationCenter.json'
 
 export const impactAppsList = [
@@ -11,18 +10,6 @@ export const impactAppsList = [
         name: 'Education Center',
         abi: FreeEducationCenterJson.abi,
         address: '0x4C60Be2cD6326137f931Ba91443f7F06EF2E2758'
-      }
-    ]
-  },
-  {
-    id: 4,
-    name: 'Global Plant Catalog',
-    mainnet: false,
-    contracts: [
-      {
-        name: 'Global Plant Catalog',
-        abi: GlobalPlantCatalogJson.abi,
-        address: '0x4d956b25c27bb91f1850c1F711843Aff72B77204'
       }
     ]
   }
