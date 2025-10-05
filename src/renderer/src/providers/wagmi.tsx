63fe8ef624854186a144daa58051d15300efd60d
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-10-04 21:22:47 -0300

    fix: unknown rpc

--- src/renderer/src/providers/wagmi.tsx
+++ src/renderer/src/providers/wagmi.tsx
@@ -4,7 +4,6 @@ import { http, WagmiProvider, createConfig } from 'wagmi'
 import { metaMask } from 'wagmi/connectors'
 import { sequoia } from '../chains/sequoia'
 import { sintrop } from '@renderer/chains/sintrop'
-import { useSettingsContext } from '@renderer/hooks/useSettingsContext'
 
 const client = new QueryClient()
 
@@ -13,15 +12,13 @@ export function WagmiProviderContainer({
 }: {
   children: React.ReactNode
 }): React.JSX.Element {
-  const { rpcUrl, sequoiaRpcUrl } = useSettingsContext()
-
   const config = createConfig({
     ssr: false,
     chains: [sintrop, sequoia],
     connectors: [metaMask()],
     transports: {
-      [sintrop.id]: http(rpcUrl),
-      [sequoia.id]: http(sequoiaRpcUrl)
+      [sintrop.id]: http(),
+      [sequoia.id]: http()
     }
   })
 
